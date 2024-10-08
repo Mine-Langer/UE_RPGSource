d7725a073b7d800768f8d41c245e4eb99a86a860
Mine-Langer <libing044@163.com>
2024-10-08 21:13:59 +0800

    Initialize

---
+++ Source/RPGSource/RPGSourceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGSourceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGSOURCE_API ARPGSourceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
